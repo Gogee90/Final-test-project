558b45809d48bdf3a155ece0b28f35038433114d
Gogee90 <dead69-not@yandex.ru>
2021-11-18 02:20:33 +0500

    Marked test for review added one test i forgot

--- test_product_page.py
+++ test_product_page.py
@@ -18,12 +18,14 @@ class TestUserAddToBasketFromProductPage:
         page.register_new_user(email, password)
         page.should_be_authorized_user()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear"
         page = ProductPage(browser, link)
         page.open()
         page.should_be_right_book_name()
 
+    @pytest.mark.need_review
     def test_user_cant_see_success_message(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
@@ -31,6 +33,7 @@ class TestUserAddToBasketFromProductPage:
         page.should_not_see_success_message_after_page_open()
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize(
     "link",
     [
@@ -87,6 +90,15 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = BasketPage(browser, link)
